4df243a27eb049ebb33007845bd6f1441471d75c
Mark McCann <Mark.McCann@glasgow.ac.uk>
2021-03-17 08:36:06 +0000

    #8 feat (text labels)
    Creating a version of the file with variable names and text value labels and outputting for SDF to read

--- 1000_raw_data_recoding_and_data_extracts.R
+++ 1000_raw_data_recoding_and_data_extracts.R
@@ -24,6 +24,8 @@ rm(list = ls())
 #    Load packages      #
 #                       #
 #########################
+library(dplyr)
+library(ggplot2)
 
 
 #########################
@@ -43,7 +45,6 @@ setwd("C:/Users/mmc78h/Documents/A Work/Net4Health/Data")
 
 test.df <- read.csv("N4H extract 12-02-2020 3 - Anonymised.csv", stringsAsFactors = FALSE)
 
-require(dplyr)
 
 #Remove a test completion
 test.df <- filter(test.df, respondent_id != "testid1")
@@ -61,7 +62,6 @@ hist(test.df$q_36_b)
 hist(test.df$q_36_c)
 hist(test.df$q_36_d)
 
-library(ggplot2)
 
 ###How _____ compared with the rest of your year group 
 #How sporty 
@@ -225,5 +225,67 @@ diet.df <- select(test.df,  c(respondent_id,
 
 
 write.csv(diet.df, file = "Net4Health Diet variables extract 16072020.csv")
+
+##########
+
+
+
+#############################################################################
+###         Create data extract for BSc Psych Medicine Project            ###
+#############################################################################
+duplicated(test.df$id)
+##########
+psychmed.df <- select(test.df,c(id
+                              , respondent_school
+                              , q_6
+                              , q_7
+                              , q_8
+                              , q_18
+                              , q_19
+                              , q_20
+                              , q_21
+                              , q_22
+                              , q_23
+                              , q_25_a, q_25_b, q_25_c, q_25_d, q_25_e, q_25_f, q_25_g, q_25_h
+                              , q_26_a, q_26_b, q_26_c, q_26_d, q_26_e, q_26_f
+                              , q_27_a, q_27_b, q_27_c, q_27_d, q_27_e, q_27_f, q_27_g, q_27_h
+                              , q_27_i, q_27_j, q_27_k
+                              , q_35_a, q_35_b, q_35_c, q_35_d, q_35_e, q_35_f, q_35_g, q_35_h
+                              , q_35_i, q_35_j, q_35_k, q_35_l, q_35_m, q_35_n, q_35_o
+                              , q_37_a, q_37_b, q_37_c, q_37_d, q_37_e, q_37_f, q_37_g
+                              , q_net_friend_1_hidden_id, q_net_friend_2_hidden_id
+                              , q_net_friend_3_hidden_id, q_net_friend_4_hidden_id
+                              , q_net_friend_5_hidden_id, q_net_friend_6_hidden_id
+                              , q_net_friend_7_hidden_id, q_net_friend_8_hidden_id
+                              , q_net_friend_9_hidden_id, q_net_friend_10_hidden_id
+                              , q_net_friend_emotional_support_outward_1_hidden_id
+                              , q_net_friend_emotional_support_outward_2_hidden_id
+                              , q_net_friend_emotional_support_outward_3_hidden_id
+                              , q_net_friend_emotional_support_inward_1_hidden_id
+                              , q_net_friend_emotional_support_inward_2_hidden_id
+                              , q_net_friend_emotional_support_inward_3_hidden_id
+                              , q_net_friend_emotional_support_outward_1_person
+                              , q_net_friend_emotional_support_outward_2_person
+                              , q_net_friend_emotional_support_outward_3_person
+                              , q_net_friend_1_support
+                              , q_net_friend_2_support
+                              , q_net_friend_3_support
+                              , q_net_friend_1_emotions
+                              , q_net_friend_2_emotions
+                              , q_net_friend_3_emotions
+                              , q_net_friend_1_makes_jokes
+                              , q_net_friend_2_makes_jokes
+                              , q_net_friend_3_makes_jokes
+                              , q_net_friend_1_changes_subject
+                              , q_net_friend_2_changes_subject
+                              , q_net_friend_3_changes_subject
+                              , q_51b_a, q_51b_b, q_51b_c, q_51b_d, q_51b_e, q_51b_f
+                              , q_51b_g, q_51b_h, q_51b_i, q_51b_j, q_51b_k, q_51b_l
+                              , q_51b_m, q_51b_n, q_51b_o, q_51b_p 
+                              , q_42_a, q_42_b, q_42_c, q_42_d, q_42_e
+                              , q_42_f, q_42_g, q_42_h, q_42_i, q_42_j
+                              , q_42_k, q_42_l
+                              ))
+write.csv(psychmed.df, file = "Net4Health Psych Med variables extract 200121.csv")
 
 ##########
